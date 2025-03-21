b3a9a5fa3a1a95527fc67a2027b6e8981cfbd125
Felix Vo <113070051+felixxvo7@users.noreply.github.com>
2025-03-21 18:21:21 -0500

    update

--- SARIMAX + SAPRC/EDA notebook.R
+++ SARIMAX + SAPRC/EDA notebook.R
@@ -10,3 +10,22 @@ plot(no2_series, main="NO2 Time Series", ylab="NO2 Concentration", xlab="Time", 
 
 adf_test <- adf.test(no2_series)
 print(adf_test)
+
+
+# Compute & Plot ACF (Autocorrelation Function)
+acf(no2_series, main="Autocorrelation Function (ACF) for NO2", lag.max=50)
+
+# Compute & Plot PACF (Partial Autocorrelation Function)
+pacf(no2_series, main="Partial Autocorrelation Function (PACF) for NO2", lag.max=50)
+
+
+# Define exogenous variables (CO, NOx, O3, T, RH)
+exog_vars <- as.matrix(data[, c("CO.GT.", "NOx.GT.", "PT08.S5.O3.", "T", "RH")])
+
+sarimax_model <- auto.arima(no2_series, xreg = exog_vars, seasonal = TRUE)
+
+summary(sarimax_model)
+
+forecast_values <- forecast(sarimax_model, xreg = exog_vars, h = 48)
+
+plot(forecast_values, main="SARIMAX Forecast for NO₂", col="blue")
